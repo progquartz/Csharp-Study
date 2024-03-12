430f5a1601ca5fdc07dc00f2cb366067ed5bf3e0
ChoiJeongYong <52409764+progquartz@users.noreply.github.com>
2024-03-12 20:50:38 +0900

    1부 C# 준비
    - 1~4챕터까지의 내용.
    - Hello World 및 기초 문법, 스타일 ,패턴 학습.

--- CsharpStudy/HelloWorld.cs
+++ CsharpStudy/HelloWorld.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 6.0 이상에서 사용 가능한 using static
+using static System.Console;
+
 namespace CsharpStudy
 {
-    class Program
+    class HelloWorld
     {
         static void Main(string[] args)
         {
+            
+            WriteLine("{0}{1}", "Hello World0!\n" , "Hello World!1");
+
+
         }
+
     }
 }
